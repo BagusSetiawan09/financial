d3ad7c5a36d9b1c2addc480842ca714d4566c4ae
bagussetiawansf <bagussetiawan.sf90@gmail.com>
2025-11-24 07:20:46 +0700

    Fix izin akses filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+// PERUBAHAN 1: Menambahkan "implements FilamentUser"
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,4 +45,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // PERUBAHAN 2: Menambahkan fungsi izin akses panel
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Hanya email ini yang boleh masuk ke Admin Panel di Production
+        return $this->email === 'bagusfinance@gmail.com';
+        
+        // Catatan: Jika nanti Anda ingin menambah admin lain, 
+        // atau ingin semua user bisa masuk, ganti baris di atas menjadi:
+        // return true;
+    }
 }
