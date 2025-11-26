a74ac716778a5595f92be1bf9dc6105fb21b0fb5
bagussetiawansf <bagussetiawan.sf90@gmail.com>
2025-11-26 20:53:45 +0700

    Add auto-fill login for demo

---
+++ app/Filament/Pages/Auth/Login.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Pages\Auth\Login as BaseLogin;
+
+class Login extends BaseLogin
+{
+    public function mount(): void
+    {
+        parent::mount();
+
+        // Automatically fill in the login form with predefined credentials
+        $this->form->fill([
+            'email' => 'bagusfinance@gmail.com',
+            'password' => 'Password',
+        ]);
+    }
+}
